a026276478362a4db9b907014a51ce7e0871df6a
Carlos Ortiz <carlosortizur@gmail.com>
2017-11-28 21:54:31 -0500

    FINAL

--- src/main/java/com/jvmt/cloudapp/domain/BlogEntry.java
+++ src/main/java/com/jvmt/cloudapp/domain/BlogEntry.java
@@ -1,8 +1,13 @@
 package com.jvmt.cloudapp.domain;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class BlogEntry {
 	private String title;
 	private String content;
+	private Date created;
+	private String id;
 	
 	
 	
@@ -10,6 +15,9 @@ public class BlogEntry {
 		super();
 		this.title = title;
 		this.content = content;
+		this.created = new Date();
+		this.id = UUID.randomUUID().toString();
+		
 	}
 
 	public String getContent() {
